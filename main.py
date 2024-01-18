4a1baae03977246e8109cacaafca04d088994307
klavsile <143964014+klavsile@users.noreply.github.com>
2024-01-18 15:43:12 +0000

    .

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+
+root = tk.Tk()
+root.title('Projekts')
+root.resizable(False, False)
+root.geometry('1280x720')
+
+#open file dialogue
+
+#convert file to gif
+
+#upload file to a site
+
+root.mainloop()
